8ff83d66033a53cd06a939a7e5645e11b408fb01
Ivan Biriukov <Grottt0@mail.ru>
2024-02-25 21:34:30 +0300

    Initial Commit

---
+++ VideoEditorUI/VideoEditorUI.h
@@ -0,0 +1,18 @@
+//
+//  VideoEditorUI.h
+//  VideoEditorUI
+//
+//  Created by иван Бирюков on 25.02.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VideoEditorUI.
+FOUNDATION_EXPORT double VideoEditorUIVersionNumber;
+
+//! Project version string for VideoEditorUI.
+FOUNDATION_EXPORT const unsigned char VideoEditorUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoEditorUI/PublicHeader.h>
+
+
